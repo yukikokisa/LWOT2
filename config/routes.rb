810092ca8febef26d36302fc486b1176ee2a1815
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2021-01-04 00:06:42 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :nurses
-  devise_for :patients
-    get 'questions/index'	
-    root to: "questions#index"	 
-    resources :questions, only: [:index]
+  devise_for :nurses, controllers: {
+    sessions: 'nurses/sessions',
+    passwords: 'nurses/passwords',
+    registrations: 'nurses/registrations'
+  }
+
+  devise_for :patients, controllers: {
+  sessions: 'patients/sessions',
+  passwords: 'patients/passwords',
+  registrations: 'patients/registrations'
+  }
+  
+  get 'questions/index'
+  root to: "questions#index"
+  resources :questions, only: [:index, :new, :create]
+  resources :checkups, only: [:new, :create]
+  resources :symptoms, only: [:new, :create]
+  resources :medical_treatments, only: [:new, :create]
+  resources :patient_lists, only: [:index, :new, :create]
 end
