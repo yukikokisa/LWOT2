1e2750059ef7d0ea5da6058a399c058b135ddec3
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2021-01-04 00:02:52 +0900

    機能実装

--- app/controllers/patients/sessions_controller.rb
+++ app/controllers/patients/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Patients::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:new, :create]
 
   # GET /resource/sign_in
   # def new
@@ -21,7 +21,7 @@ class Patients::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+   devise_parameter_sanitizer.permit(:sign_in, keys: [:room_number])
+  end
 end
