57cfa8ed84123ad0822d0acda496a7035e87a2f6
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2021-01-03 22:59:57 +0900

    モデルの生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :nurses
+  devise_for :patients
     get 'questions/index'	
     root to: "questions#index"	 
     resources :questions, only: [:index]
