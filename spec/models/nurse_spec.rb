053e74404bc1dc819c6bdb5dba10c5423d6091c6
yukikokisa <yukikokisa@kisayukikonoMacBook-Pro.local>
2021-02-19 23:00:51 +0900

    Nurseモデル単体テストコード

--- spec/models/nurse_spec.rb
+++ spec/models/nurse_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Nurse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nameが空では登録できない' do
+      nurse = Nurse.new(name: '')
+      nurse.valid?
+      expect(nurse.errors.full_messages).to include("Name can't be blank")
+    end
+    it 'employee_numberが空では登録できない' do
+      nurse = Nurse.new(employee_number: '')
+      nurse.valid?
+      expect(nurse.errors.full_messages).to include("Employee number can't be blank")
+    end
+    it 'employee_numberが8桁でなければ登録できない' do
+      nurse = Nurse.new(employee_number: 000000)
+      nurse.valid?
+      expect(nurse.errors.full_messages).to include("Employee number is the wrong length (should be 8 characters)")
+    end
+  end
 end
